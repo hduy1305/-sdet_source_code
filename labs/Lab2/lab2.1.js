635bc468310ef2c01fcc3b8320f50795c40ac4ae
hduy1305 <kenzyduy8@gmail.com>
2025-07-02 18:30:50 +0700

    done

--- labs/Lab2/lab2.1.js
+++ labs/Lab2/lab2.1.js
@@ -1,14 +1,14 @@
 const readline = require("readline-sync");
 let height = Number(readline.question("Enter your height in meters: "));
-let weight = NUmber(readline.question("Enter your wright in kilograms: "));
-let bmi = weight / height * height;
+let weight = Number(readline.question("Enter your wright in kilograms: "));
+let bmi = weight / (height * height);
 
 console.log("Your BMI is: " + bmi);
 if(bmi < 18.5){
     console.log("You are UNDERWEIGHT");
 }else if(bmi<24.9){
     console.log("You are NORMAL");
-}else if(nmi<29.9){
+}else if(bmi<29.9){
     console.log("You are OVERWEIGHT");
 }else{
     console.log("You are OBESITY");
--- labs/Lab2/lab2.3.js
+++ labs/Lab2/lab2.3.js
@@ -1,35 +1,38 @@
 const readline = require("readline-sync");
-let height = Number(readline.question("Enter your height in meters: "));
-let weight = Number(readline.question("Enter your wright in kilograms: "));
-let bmi = weight / (height * height);
+let height = Number(readline.question("Enter your height in meters: ")).toFixed(2);
+let weight = Number(readline.question("Enter your wright in kilograms: ")).toFixed(2);
+let bmi = (weight / (height * height)).toFixed(2);
 console.log("Your BMI is: " + bmi);
 if(bmi < 18.5){
     console.log("You are UNDERWEIGHT");
-    let leastWeightIncrease = 18.5*height*height;
-    let reasonableWeightMin = 22*height*height;
-    let reasonableWeightMax = 24*height*height;
-    console.log("You should increase atleast " + leastWeightIncrease - weight + "kilos.");
+    let leastWeightIncrease = (18.5*height*height).toFixed(2);
+    let reasonableWeightMin = (22*height*height).toFixed(2);
+    let reasonableWeightMax = (24*height*height).toFixed(2);
+    let toIncrease = leastWeightIncrease - weight;
+    console.log("You should increase atleast " + toIncrease.toFixed(2) + "kilos.");
     console.log("And then keep yourself around " + reasonableWeightMin + " to " + reasonableWeightMax + ".");
     
 }else if(bmi<24.9){
     console.log("You are NORMAL");
-    let reasonableWeightMin = 22*height*height;
-    let reasonableWeightMax = 24*height*height;
+    let reasonableWeightMin = (22*height*height).toFixed(2);
+    let reasonableWeightMax = (24*height*height).toFixed(2);
     console.log("You are good. Stay fit");
     if(weight < reasonableWeightMin || weight > reasonableWeightMax) console.log("But i recommend to keep yourself around " + reasonableWeightMin + " to " + reasonableWeightMax + " is the best. ");
-}else if(nmi<29.9){
+}else if(bmi<29.9){
     console.log("You are OVERWEIGHT");
-    let leastWeightDecrease = 24.89*height*height;
-    let reasonableWeightMin = 22*height*height;
-    let reasonableWeightMax = 24*height*height;
-    console.log("You should decrease atleast " + weight - leastWeightDecrease + "kilos." );
+    let leastWeightDecrease = (24.89*height*height).toFixed(2);
+    let reasonableWeightMin = (22*height*height).toFixed(2);
+    let reasonableWeightMax = (24*height*height).toFixed(2);
+    let toDecrease = weight - leastWeightDecrease;
+    console.log("You should decrease atleast " + toDecrease.toFixed(2) + " kilos." );
     console.log("After that you can maintain your weight around " + reasonableWeightMin + " to " + reasonableWeightMax + " is the best. ");
 }else{
     console.log("You are OBESITY");
-    let leastWeightDecrease = 24.89*height*height;
-    let reasonableWeightMin = 22*height*height;
-    let reasonableWeightMax = 24*height*height;
-    console.log("You should decrease atleast " + weight - leastWeightDecrease + "kilos to stay normal." );
+    let leastWeightDecrease = (24.89*height*height).toFixed(2);
+    let reasonableWeightMin = (22*height*height).toFixed(2);
+    let reasonableWeightMax = (24*height*height).toFixed(2);
+    let toDecrease = (weight - leastWeightDecrease);
+    console.log("You should decrease atleast " + toDecrease + "kilos to stay normal." );
     if(weight - leastWeightDecrease > 50){
         console.log("I see that is alot of weight to decrease in a short term but not impossible. Keep track of it.");
         
