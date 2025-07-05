4e6f3a742b29cca729fa33fab4e48f25d1f9b67d
hduy1305 <kenzyduy8@gmail.com>
2025-07-06 00:06:41 +0700

    improved and fixed lab2

--- labs/Lab2/lab2.3.js
+++ labs/Lab2/lab2.3.js
@@ -1,6 +1,23 @@
 const readline = require("readline-sync");
-let height = Number(readline.question("Enter your height in meters: ")).toFixed(2);
-let weight = Number(readline.question("Enter your wright in kilograms: ")).toFixed(2);
+
+let height ;
+let weight ;
+do{
+    let input = Number(readline.question("Enter your height in meters: "));
+    height = input;
+    if(isNaN(height) || height<0.15 || height > 3) console.log("Please enter a valid height");
+    else break;
+}while(isNaN(height));
+
+do{
+    let input = Number(readline.question("Enter your wright in kilograms: "));
+    weight = input;
+
+    if(isNaN(weight) || weight<2 || height > 450) console.log("Please enter a valid weight");
+    else break;
+
+}while(isNaN(weight));
+
 let bmi = (weight / (height * height)).toFixed(2);
 console.log("Your BMI is: " + bmi);
 if(bmi < 18.5){
