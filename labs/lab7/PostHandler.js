79c2b3ae1363760ce9991a588407598e8c3c1be9
hduy1305 <127862604+hduy1305@users.noreply.github.com>
2025-09-02 17:56:18 +0700

    lab7

--- labs/lab7/PostHandler.js
+++ labs/lab7/PostHandler.js
@@ -1,8 +1,36 @@
-const post = require("./PostModel");
-const requestHandler = require("./RequestHandler");
+const Post = require("./PostModel");
+const RequestHandler = require("./RequestHandler");
+const readline = require("readline-sync");
 
 lab11();
 
 async function lab11(){
+    message();
+    let userOption = getUserInput();
+    let requestHandler = new RequestHandler();
 
+    if(userOption === "1"){
+        let userId = Number(readline.question("Input userId: "));
+        console.log(await requestHandler.getAllPosts(userId));
+    }else if(userOption === "2"){
+        let userId = Number(readline.question("Input userId: "));
+        let postId = Number(readline.question("Input postId: "));
+        let user = await requestHandler.getTargetPost(userId, postId);
+        if(user !== null) console.log(`Title: ${user._title}\n Content: ${user._body}`);
+        else console.log("Not Found");
+    }else{
+        console.log(`No option was found!`)
+    }
 }
+
+function message(){
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    console.log(`This lab get you user's list posts \nand one post you want to get from that user with this API: \n ${url}`);
+}
+
+function getUserInput(){
+    console.log(` 1. Get all Posts from User\n 2. Get 1 specific Post from User`);
+    let userInput = readline.question("Input your option: ");
+    return userInput;
+}
+
--- labs/lab7/RequestHandler.js
+++ labs/lab7/RequestHandler.js
@@ -1,10 +1,34 @@
+const Post = require("./PostModel");
+
 class RequestHandler{
     async getTargetPost(userId, postId) {
-        // Construct the returned data as a Post data model from class Post
+        let userFetch = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+        if(userFetch.ok === false){
+            return null;
+        }
+        
+        let filterPost = await this.getAllPosts(userId);
+
+        let postModel = new Post();
+        for(let post of filterPost){
+            if(post.id === postId){
+                postModel.title = post.title;
+                postModel.body = post.body;
+                break;
+            }
+        }
+
+        return postModel;
     }
 
     async getAllPosts(userId) {
         // Construct the returned data as a [ Post data model ] from class Post
+        let postFetch = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+        let allPosts = await postFetch.json()
+        let filterPost = allPosts.filter(function (post){
+            return post.userId === userId;
+        });
+        return filterPost;
     }
 
 }
